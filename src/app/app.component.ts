1be8ac6a10334b098416f80ac97def263061af1a
welcomesustain <45425187+welcomesustain@users.noreply.github.com>
2021-04-01 19:50:03 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tempup';
+  title = 'Calculator App';
+  num1: number;
+  num2: number;
+  result: number;
+  add() {
+      this.result = this.num1 + this.num2;
+  }
+  substract() {
+      this.result = this.num1 - this.num2;
+  }
+  multiply() {
+      this.result = this.num1 * this.num2;
+  }
+  divide() {
+      this.result = this.num1 % this.num2;
+  }
+
 }
+
